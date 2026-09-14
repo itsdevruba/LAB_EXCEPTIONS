f34c959d02b05795d58a1f33a1d793d1dcc8306d
itsdevruba <rubaaldawssari@gmail.com>
2026-09-14 17:49:46 +0300

    find the exp

---
+++ Math_code.py
@@ -0,0 +1,11 @@
+def additoin(x, y):
+    x = 10
+    y = 20
+    print("Addition:", x + b)
+
+try:
+    additoin(10, 20)
+except NameError:
+    print("the name is not defined")
+else:
+    print("the operation is successful")
